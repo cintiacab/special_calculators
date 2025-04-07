7c8171f45fc55afd14341b01452cd1be7ec100e5
Cintia Cabral <cintiacabral@hotmail.com>
2025-04-07 13:34:22 -0300

    Including Interfaces to the Project + calculator 2 route

--- src/calculators/calculator_2.py
+++ src/calculators/calculator_2.py
@@ -1,8 +1,10 @@
 from typing import Dict, List
 from flask import request as FlaskResquest
-from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 
 class Calculator2:
+    def __init__(self, driver_handler: DriverHandlerInterface) -> None:
+        self.__driver_handler = driver_handler
 
     def calculate(self, request: FlaskResquest) -> Dict:  # type: ignore
         body = request.json
@@ -19,15 +21,14 @@ class Calculator2:
         return input_data
     
     def __process_data(self, input_data: List[float]) -> float:
-        numpy_handler = NumpyHandler()
         first_process = [(num * 11) ** 0.95 for num in input_data]
-        result = numpy_handler.standard_derivation(first_process)
+        result = self.__driver_handler.standard_derivation(first_process)
         return 1/result
         
     def __format_response(self, calculated_number: float) -> Dict:
         return {
             "data" : {
                 "Calculator": 2,
-                "result" : round (calculated_number, 2).item()
+                "result" : round (calculated_number, 2)
             }
         }
--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,13 +1,32 @@
-from typing import Dict
+from typing import Dict, List
 from .calculator_2 import Calculator2
+from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
         self.json = body
 
+class MockDriverHandler(DriverHandlerInterface):
+    def standard_derivation(self, numbers: List[float]) -> float:
+        return 3
+
 def test_calculate():
     mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
-    calculator_2 = Calculator2()
+    driver = MockDriverHandler()
+    calculator_2 = Calculator2(driver)
+    response = calculator_2.calculate(mock_request)
+    print()
+    print(response)
+
+    assert isinstance(response, dict) 
+    assert response == {'data': {'Calculator': 2, 'result': (0.33)}}   
+
+# Integration between NumpyHandler and Calculator2 (not a single test)
+def test_calculate_integration():
+    mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
+    driver = NumpyHandler()
+    calculator_2 = Calculator2(driver)
     response = calculator_2.calculate(mock_request)
     print()
     print(response)
